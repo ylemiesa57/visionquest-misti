a07b01428ec4407f924dcb73b0a27abffa368b74
Yaphet Lemiesa <79425196+ylemiesa57@users.noreply.github.com>
2024-12-11 04:38:01 -0500

    Create day3.py

---
+++ Week 1/day3.py
@@ -0,0 +1,24 @@
+#Day 3: Intro to Numpy
+# Video Link: [Insert Link Here]
+# Documentation here for more functions and things to try
+# Challenge problem
+
+"""
+Content:
+- Array creation, manipulation, and operations using NumPy.
+- Understanding image data with NumPy.
+"""
+
+import numpy as np
+
+# Example: Array Creation
+array = np.array([1, 2, 3, 4, 5])
+print(array)
+
+# Example: Basic Operations
+array2 = np.array([5, 4, 3, 2, 1])
+print(array + array2)
+
+# Practice: Create a 2D NumPy array and find the sum of its elements.
+matrix = np.array([[1, 2, 3], [4, 5, 6]])
+print(np.sum(matrix))
